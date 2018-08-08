ed806d5394b7b34bde9875eb63921b76b2431e21
rummolprod999 <rummolprod999@gmail.com>
2018-08-08 16:59:15 +0300

    update

--- parser_icetrade.go
+++ parser_icetrade.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	_ "github.com/go-sql-driver/mysql"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -27,18 +28,43 @@ type TenderIcetrade struct {
 func (t *ParserIcetrade) parsing() {
 	defer SaveStack()
 	Logging("Start parsing")
-	for _, p := range t.Urls {
-		for i := 1; i <= PagesIcetrade; i++ {
-			urllist := fmt.Sprintf("%s%s", p, fmt.Sprintf("%d&onPage=100", i))
-			t.parsingPage(urllist)
-		}
+	var today = time.Now()
+	oneDay := time.Hour * -24
+	yesterday := today.Add(oneDay)
+	yesterdayS := yesterday.Format("02.01.2006")
+	startUrl := fmt.Sprintf("http://www.icetrade.by/search/auctions?search_text=&zakup_type[1]=1&zakup_type[2]=1&auc_num=&okrb=&company_title=&establishment=0&industries=&period=&created_from=%s&created_to=%s&request_end_from=&request_end_to=&t[Trade]=1&t[eTrade]=1&t[socialOrder]=1&t[singleSource]=1&t[Auction]=1&t[Request]=1&t[contractingTrades]=1&t[negotiations]=1&t[Other]=1&r[1]=1&r[2]=2&r[7]=7&r[3]=3&r[4]=4&r[6]=6&r[5]=5&sort=num%%3Adesc&sbm=1&onPage=20&p=", yesterdayS, yesterdayS)
+	countPage := t.getCountPage(startUrl)
+	for i := 1; i <= countPage; i++ {
+		ul := fmt.Sprintf("%s%d", startUrl, i)
+		t.parsingPage(ul)
+		time.Sleep(5 * time.Second)
 	}
-
 	Logging("End parsing")
 	Logging(fmt.Sprintf("Добавили тендеров %d", AddtenderPhosagro))
 	Logging(fmt.Sprintf("Обновили тендеров %d", UpdatetenderPhosagro))
 }
-
+func (t *ParserIcetrade) getCountPage(url string) int {
+	c := 10
+	u := fmt.Sprintf("%s%d", url, 1)
+	defer SaveStack()
+	r := DownloadPage(u)
+	if r != "" {
+		doc, err := goquery.NewDocumentFromReader(strings.NewReader(r))
+		if err != nil {
+			Logging(err)
+			return c
+		}
+		numP := strings.TrimSpace(doc.Find("div.paging a").Last().Text())
+		i, err := strconv.Atoi(numP)
+		if err != nil {
+			return c
+		}
+		return i
+	} else {
+		Logging("Получили пустую строку", u)
+	}
+	return c
+}
 func (t *ParserIcetrade) parsingPage(p string) {
 	defer SaveStack()
 	r := DownloadPage(p)
@@ -58,7 +84,7 @@ func (t *ParserIcetrade) parsingTenderList(p string, url string) {
 	}
 	doc.Find("#auctions-list tbody tr[class^='rw']").Each(func(i int, s *goquery.Selection) {
 		t.parsingTenderFromList(s, url)
-
+		time.Sleep(5 * time.Second)
 	})
 }
 
